0f7ac5730162a79a0fc3109f0052b91d62b0d57d
MierafA12 <mierafabebe12@gmail.com>
2024-09-22 17:32:43 +0300

    updated the css and other things

--- src/Component-jsx/Header.jsx
+++ src/Component-jsx/Header.jsx
@@ -1,36 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import "../Component-css/Header.css";
 
 export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
- <div className='header'> 
-   <div className='name'>
-     <div className='maf'></div>
-   </div>
-   <div className='links'>
-    <div className='one-link'>
-    <div className='act'><Link className="link" to="/">
-         <p> Home</p>
-    </Link></div>
-    </div>
-    <div className='one-link'>
-    <Link className="link" to="/About">
-         <p>About</p>
-    </Link>
-       
-    </div>
-    <div className='one-link'>   
-     <Link className="link" to="/Portfolio">
-         <p>Portfolio</p>
-    </Link>
-    </div>
-    <div className='one-link'>
-    <Link className="link" to="/contact">
-         <p>contact</p>
-    </Link>
-    </div>
-   </div> 
- </div>
+        <div className='header'>
+            <div className='name'>
+                <div className='maf'></div>
+            </div>
+
+            {/* Hamburger Icon */}
+            <div className={`hamburger ${isMenuOpen ? "open" : ""}`} onClick={toggleMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+
+            {/* Links (shown or hidden based on isMenuOpen state) */}
+            <div className={`links ${isMenuOpen ? "show" : ""}`}>
+                <div className='one-link'>
+                    <div className='act'>
+                        <Link className="link" to="/" onClick={toggleMenu}>
+                            <p>Home</p>
+                        </Link>
+                    </div>
+                </div>
+                <div className='one-link'>
+                    <Link className="link" to="/About" onClick={toggleMenu}>
+                        <p>About</p>
+                    </Link>
+                </div>
+                <div className='one-link'>
+                    <Link className="link" to="/Portfolio" onClick={toggleMenu}>
+                        <p>Portfolio</p>
+                    </Link>
+                </div>
+                <div className='one-link'>
+                    <Link className="link" to="/contact" onClick={toggleMenu}>
+                        <p>Contact</p>
+                    </Link>
+                </div>
+            </div>
+        </div>
     );
 }
